af8ddd5586e3257fab4860d579ceb9c37a748646
Vincent Lau <lauchunyin@gmail.com>
2014-08-07 22:28:36 +0800

    bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var lr = require('tiny-lr');
+var gulp = require('gulp');
+var gulputil = require('gulp-util');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var bower = require('gulp-bower');
+var usemin = require('gulp-usemin');
+
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+
+
+var minifyCss = require('gulp-minify-css');
+
+server = lr();
+
+var publicFolder = 'public/';
+
+gulp.task('bower', function() {
+    bower()
+        .pipe(gulp.dest(publicFolder + 'lib/'));
+});
+
+gulp.task('imagemin',function() {
+    gulp.src('public/images/*')
+        .pipe(imagemin({
+            optimizationLevel:3,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('dist/images/'));
+
+      gulp.src('public/data/*png')
+        .pipe(imagemin({
+            optimizationLevel:3,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('dist/data/'));
+
+             
+})
+
+var filesToMove = [
+        publicFolder+'data/**/*.*',
+        // publicFolder+'images/**/*.*',
+        publicFolder+'templates/**/*.*'
+    ];
+
+gulp.task('usemin', function(){
+  gulp.src(publicFolder+'index.html')
+    .pipe(usemin({
+         css: [minifyCss(), 'concat'],
+      js: [uglify()]
+      // in this case css will be only concatenated (like css: ['concat']).
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy',function() {
+    gulp.src(filesToMove, { base: publicFolder })
+  .pipe(gulp.dest('dist'));
+
+})
+
+gulp.task('build',['bower','usemin','copy']);
+//Step:
+// merge-geojson-features
+
+gulp.task('default', ['bower', 'listen'], function() {
+    gulp.src(publicFolder + '*')
+        .pipe(watch())
+        .pipe(livereload(server));
+        
+    // gulp.src('./scss/*.scss')
+    //     .pipe(watch())
+    //     .pipe(sass())
+    //     .pipe(gulp.dest(publicFolder + 'styles/'))
+    //     .pipe(livereload(server));
+});
+
+gulp.task('listen', function(next) {
+    server.listen(35729, function(err) {
+        if (err) return console.error(err);
+        next();
+    });
+});
+
---
+++ public/scripts/app.js
@@ -0,0 +1,68 @@
+'use strict';
+// Declare app level module which depends on filters, and services
+angular.module('recycle4hk', [
+    'ngRoute',
+    'ngAnimate',
+    'ui.bootstrap',
+    'ui.bootstrap.carousel',
+    'snap'
+]).
+config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/timeline', {
+            templateUrl: 'templates/timeline.html',
+            controller: 'TimelineCtrl',
+            reloadOnSearch: false
+        });
+        $routeProvider.when('/', {
+            templateUrl: 'templates/landing.html',
+            controller: 'LandingCtrl',
+            reloadOnSearch: false
+        });
+        $routeProvider.when('/visualize', {
+            templateUrl: 'templates/visualization.html',
+            controller: 'VisualizationCtrl',
+            reloadOnSearch: false
+        });
+        $routeProvider.when('/visualize-map', {
+            templateUrl: 'templates/visualization.html',
+            controller: 'VisualizationCtrl',
+            reloadOnSearch: false
+        });
+
+        $routeProvider.when('/visualize-composition', {
+            templateUrl: 'templates/visualization-composition.html',
+            controller: 'VisualizationCtrl',
+            reloadOnSearch: false
+        });
+
+                $routeProvider.when('/tips', {
+            templateUrl: 'templates/tips.html',
+            controller: 'VisualizationCtrl',
+            reloadOnSearch: false
+        });
+
+
+
+        https://dchtm6r471mui.cloudfront.net/hackpad.com_USVAwLGvyQC_p.144570_1404570508252_undefined
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
+])
+.controller('TimelineCtrl', ['$scope',
+    function($scope) {
+        console.log('Timeline');
+    }
+])
+.controller('LandingCtrl', ['$scope',
+    function($scope) {
+        console.log('Timeline');
+    }
+])
+.controller('VisualizationCtrl', ['$scope',
+    function($scope) {
+        console.log('VisualizationCtrl');
+    }
+])
+
